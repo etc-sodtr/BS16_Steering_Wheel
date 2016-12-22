1cb8afa4e503cf396885426c268bbc826d3496f2
lupuvvlad <lupuv.vlad@yahoo.com>
2016-12-22 12:58:20 +0200

    Add files via upload
    This is the Blink a led program for AT90CAN

---
+++ AT90FreeRTOS/AT90FreeRTOS/AT90FreeRTOS/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+source\heap_1.c
+
+source\list.c
+
+source\main.c
+
+source\port.c
+
+source\queue.c
+
+source\tasks.c
+
---
+++ AT90FreeRTOS/AT90FreeRTOS/AT90FreeRTOS/source/main.c
@@ -0,0 +1,56 @@
+/*
+ * leds.c
+ *
+ * Created: 17-Dec-16 16:23:24
+ * Author : lupuv
+ */ 
+
+
+// include AVR headers
+#include <avr/io.h>
+
+// include the operating system
+#include "../include/FreeRTOS.h"
+#include "../include/task.h"
+#include "../include/BitOperations_V1.5.h"
+// tasks
+static void blink( void *pvParameters );
+
+
+/*-----------------------------------------------------------*/
+
+int main( void )
+{
+	
+	// initialize port
+	DDRG=0x01;
+	PORTG = 0x01;
+	
+	
+
+	// create blinking task
+	xTaskCreate( blink, "Blink", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+
+	// start the scheduler
+	vTaskStartScheduler();
+
+	return 0;
+}
+/*-----------------------------------------------------------*/
+
+static void blink( void *pvParameters )
+{
+	//uint8_t dir = 1; // direction flag
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	const TickType_t delay = 500;
+
+	(void)pvParameters; // parameters not used
+
+	// forever loop
+	for(;;)
+	{
+		vTaskDelayUntil(&xLastWakeTime, delay);
+
+TOGGLE_PIN(G,0);
+	}
+}
